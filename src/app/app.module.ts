b2ba7af5af0d8756f490b84668ed80ec8e15479b
kachun <kcwong8051@gmail.com>
2022-05-07 09:37:40 +0800

    first commit for working with node.js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StudentFormComponent } from './student-form/student-form.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/student-form/student-form.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+//import { Http, Response } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-student-form',
+  templateUrl: './student-form.component.html',
+  styleUrls: ['./student-form.component.css']
+})
+export class StudentFormComponent implements OnInit {
+
+  studentForm: FormGroup;
+  serverData: Object;
+  loading: boolean=false;
+  http: HttpClient;
+
+  constructor(fb: FormBuilder, http: HttpClient) { 
+    this.http = http;
+    this.studentForm = fb.group(   
+        {
+          'studentInput': [ '190000000', Validators.compose( [Validators.required, this.studentIdValidator] ) ],  // studentForm.controls[ 'studentInput' ]
+          'studentName': [ 'Your name' , Validators.required ],      // studentForm.controls[ 'studentName' ]
+          'studentEmail': [ 'name@your.domain', Validators.email ]
+        }
+    );
+    this.serverData = {};
+  }
+
+  studentIdValidator(control: FormControl): { [s: string]: boolean } {
+    if (!control.value.match(/^19[0-9]{7}/)) {
+      return {invalidStudentId: true};
+    }
+    return {invalidStudentId: false};
+  }
+  
+
+  onSubmit(formValue: any): void {
+    console.log( 'you submitted value: ', formValue );
+
+    if (this.studentForm.valid) {
+        console.log( 'you submitted value: ', formValue );
+        this.loading = true;
+        this.serverData = {};
+        // var pathName = '/api/student/'+formValue['studentInput']; 
+        var pathName = '/api/student/' +
+          this.studentForm.controls['studentInput'].value + '/' +
+          this.studentForm.controls['studentName'].value + '/' +
+          this.studentForm.controls['studentEmail'].value;
+        console.log('URL: ' + pathName);        
+        this.http.get(pathName).subscribe({      
+            next: (res) => {
+              console.log("data is fetched successfully.");
+              this.serverData = JSON.parse(JSON.stringify(res)); //res.json();
+              this.loading = false;            
+            },
+            error: (err: string) => {
+              console.log("Server call failed: " + err);
+            }
+          })
+        
+    } else
+        console.log( 'errors in form');
+
+  }
+
+  ngOnInit() {
+  }
+
+}
